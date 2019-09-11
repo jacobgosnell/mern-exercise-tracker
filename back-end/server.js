d0805e80940f3a427c8d68eea9e374489eb9e101
Jake <gosnelljacob@gmail.com>
2019-09-10 23:01:09 -0500

    added models and routes folders

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,6 +18,12 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
